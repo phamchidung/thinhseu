43cb67ee9dabaacfb5d5854a565b2c9c5e61a0ca
Phạm Chí Dũng <dung6app@gmail.com>
2021-05-30 17:48:09 +0700

    final-01

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+// 'use strict';
+
+// var gulp = require('gulp');
+// var sass = require('gulp-sass');
+// var browserSync = require('browser-sync');
+
+// gulp.task('sass', function () {
+//     return gulp.src('./css/*.scss')
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(gulp.dest('./css'));
+// });
